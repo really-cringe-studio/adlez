da7ad75add6939508e673f93b64a5c24167691df
Patrick Gingras <ging0044@algonquinlive.com>
2018-04-14 20:05:07 -0400

    removed meta files

--- Game1.cs
+++ Game1.cs
@@ -7,12 +7,12 @@ namespace Adlez
     /// <summary>
     /// This is the main type for your game.
     /// </summary>
-    public class Game1 : Game
+    public class Adlez : Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         
-        public Game1()
+        public Adlez()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
